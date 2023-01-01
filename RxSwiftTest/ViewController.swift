24d1e81490ee6f907103d491406d5248f28da97f
Artem Galay <iostema@mail.ru>
2023-01-01 22:52:51 +0300

    Initial Commit

---
+++ RxSwiftTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftTest
+//
+//  Created by Артем Галай on 1.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
